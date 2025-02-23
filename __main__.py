0545cc5b67a95bc97c1ffcc3058d726455eab35c
Edoardo Zerbo <edoardo.zerbo@gmail.com>
2025-02-23 20:51:45 +0100

    new_event_loop instead of get_event_loop

--- __main__.py
+++ __main__.py
@@ -59,5 +59,5 @@ async def main():
 
 # Entry point of the script
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
+    loop = asyncio.new_event_loop()
     loop.run_until_complete(main())
